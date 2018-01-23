5f1cdfb3177d20916f3c15e81e819224ae2304d3
ELMAHDI-AR <el-mahdi.archane@hotmail.com>
2018-01-22 20:57:56 -0800

    DemoScheduler

---
+++ SchedulerV/Scheduler.Data/Abstract/IRepositories.cs
@@ -0,0 +1,11 @@
+﻿using Scheduler.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scheduler.Data.Abstract
+{
+    public interface IScheduleRepository : IEntityBaseRepository<Schedule> { }
+    public interface IUserRepository : IEntityBaseRepository<User> { }
+    public interface IAttendeeRepository : IEntityBaseRepository<Attendee> { }
+}
